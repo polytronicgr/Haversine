4a1c767a27ec2c006fe321d44979e2d24f8f4da4
Mark Carr <sparky.carr@hotmail.co.uk>
2018-04-28 08:04:22 +0100

    Initial commit.

---
+++ Haversine.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Haversine.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Haversine!");
+
+            Console.WriteLine("Press a key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
